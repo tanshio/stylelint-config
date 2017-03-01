5f3eb5311ca351e0551d5b3618076b2247dd65db
tanshio <rakuburomail+tanshio@gmail.com>
2017-03-02 02:53:01 +0900

    base

--- test/test.js
+++ test/test.js
@@ -3,13 +3,17 @@ const test = require('ava');
 const stylelint = require('stylelint');
 const config = require("../");
 
-const css = `
+
+
+
+const inValidcss = `
 /**
  * Multi-line comment
  */
 
 .test {
-  pading-left: 10px;
+  ml: 20px;
+  padding-left: 10px;
   padding: 20px;
 }
 
@@ -22,24 +26,72 @@ const css = `
   display: block;
   color: #333;
 }
+
+
+
+
+.selector-4{color:red}
+@media screen and (max-width:991px){
+  .selecter-5 {
+    color: blue;
+  }
+}
+`;
+
+const validcss = `
+/**
+ * Multi-line comment
+ */
+
+.test {
+  margin-left: 20px;
+  padding: 20px;
+  padding-left: 10px;
+}
+
+
+.selector-1,
+.selector-2,
+.selector-3[type="text"] {
+  background: linear-gradient(#fff, rgba(0, 0, 0, 0.8));
+  box-sizing: border-box;
+  display: block;
+  color: #333;
+}
+
+.selector-4 {
+  color: red;
+}
+
+@media screen and (max-width: 991px) {
+  .selecter-5 {
+    color: blue;
+  }
+}
 `;
 
 
 
 test('foo',async t => {
   const result = stylelint.lint({
-      code: css,
+      code: inValidcss,
       config,
     })
-  // console.log(result)
 
   await result.then(data => {
-    // console.log(data)
-    t.falsy(data.errored, data.warnings)
+    const errored = data.errored;
+    t.false(errored, data.results[0].warnings.map(e=>e.text).join('\n'))
   });
+})
 
-});
+test('bar',async t => {
+  const result = stylelint.lint({
+      code: validcss,
+      config,
+    })
 
-test('bar',t => {
-  t.deepEqual([1, 2], [2, 2]);
-});
+  await result.then(data => {
+    const errored = data.errored;
+    t.false(errored, data.results[0].warnings.map(e=>e.text).join('\n'))
+  });
+})
